41f375d46c5453698bf9af97331efab86dccab91
Bram van Gils <BramvGilsDeveloper@gmail.com>
2021-11-14 00:31:20 +0100

    Started work on grimace implementation with audio

--- Assets/Grimace.cs
+++ Assets/Grimace.cs
@@ -3,7 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(AudioSource))]
 public class Grimace : MonoBehaviour
 {
-    
+    private AudioSource audioSource;
+
+    [SerializeField] private AudioClip[] whispers;
+    [SerializeField] private AudioClip gasp;
+
+
+    private void Awake()
+    {
+        audioSource = GetComponent<AudioSource>();
+
+    }
+
+    private void Update()
+    {
+        if (!audioSource.isPlaying)
+        {
+            AudioClip clip = whispers[Random.Range(0, whispers.Length - 1)];
+            audioSource.clip = clip;
+            audioSource.Play();
+        }
+    }
 }
